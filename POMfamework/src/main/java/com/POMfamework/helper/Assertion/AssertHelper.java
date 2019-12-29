ae05005167ffdea0888ea363be9e5f62869813c2
meetparag81 <paragborawake81@gmail.com>
2019-12-29 16:28:48 +0530

    Assert and Alertclasses added inframework

--- POMfamework/src/main/java/com/POMfamework/helper/Assertion/AssertHelper.java
+++ POMfamework/src/main/java/com/POMfamework/helper/Assertion/AssertHelper.java
@@ -2,6 +2,7 @@ package com.POMfamework.helper.Assertion;
 
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 
 import com.POMfamework.helper.logger.LoggerHelper;
@@ -9,15 +10,17 @@ import com.POMfamework.helper.logger.LoggerHelper;
 public class AssertHelper 
 {
 	
-	private Logger log = LoggerHelper.GetLogger(AssertHelper.class);
+	private static  Logger log = LoggerHelper.GetLogger(AssertHelper.class);
 
 	
 	public static void VerifyText(String s1, String s2)
 	{
+		log.info("veriying Sting"+s1 + "with" + s2);
 		Assert.assertEquals(s1, s2);
 	}
 	public static void VerifyTrue()
 	{
+		log.info("making script pass");
 		Assert.assertTrue(true);
 	}
 	public static void VerifyTrue(String message)
@@ -26,10 +29,18 @@ public class AssertHelper
 	}
 	public static void VerifyFalse()
 	{
+		log.info("making script fail");
 		Assert.assertFalse(false);
 	}
 	public static void VerifyFalse(String message)
 	{
 		Assert.assertTrue(false,message);
 	}
+	
+	public static void VerifyNullObject(String s1)
+	{
+		log.info("Object is null");
+		Assert.assertNull(s1);
+	}
+	
 }
