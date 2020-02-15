e4e4fce31dcba0c10dfebcf1ee005f11979a8bb3
meetparag81 <paragborawake81@gmail.com>
2020-02-15 22:20:44 +0530

    chromedriver

---
+++ POMfamework/src/main/java/com/POMfamework/helper/browserConfiguration/ChromeBrowser.java
@@ -0,0 +1,26 @@
+package com.POMfamework.helper.browserConfiguration;
+
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class ChromeBrowser 
+{
+	
+	public ChromeOptions getChromeOptions()
+	{
+		ChromeOptions option = new ChromeOptions();
+		option.addArguments("--test-type");
+		option.addArguments("--disable-popup-blocking");
+		DesiredCapabilities chrome = new DesiredCapabilities().chrome();
+		chrome.setJavascriptEnabled(true);
+		option.setCapability(ChromeOptions.CAPABILITY, option);
+		if(System.getProperty("osname").contains("Linux")
+				{
+			
+				}
+		
+ 		return option;
+		
+	}
+
+}
